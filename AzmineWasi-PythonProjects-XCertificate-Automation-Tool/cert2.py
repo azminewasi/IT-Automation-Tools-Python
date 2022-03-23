bacfb44fce1eb6f4153de8f74868a39a004adcdc
Azmine Toushik Wasi <azmine.wasi@gmail.com>
2022-03-23 21:03:16 +0600

    with folder creation

--- AzmineWasi-PythonProjects-XCertificate-Automation-Tool/cert2.py
+++ AzmineWasi-PythonProjects-XCertificate-Automation-Tool/cert2.py
@@ -1,8 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import pandas as pd
 import cv2
-
-
+import os
 
 
 
@@ -19,8 +18,6 @@ fontNtext=ImageFont.truetype(font_name, 80)
 
 
 
-
-
 form = pd.read_excel(excel_file_name)
 name_list = form['Name'].to_list()
 mail_list = form['Mail'].to_list()
@@ -29,6 +26,8 @@ img = cv2.imread(cert_demo)
 MAX_H,MAX_W,r = img.shape
 current_h, pad = (MAX_H/2)-height_adjustment, 10
 
+os.mkdir("pdfs")
+os.mkdir("pngs")
 
 print(img.shape)
 total = len(name_list)
@@ -49,7 +48,7 @@ while (count<total):
     w, h = d.textsize(i, font=font)
     location = ((MAX_W - w) / 2, current_h)
     d.text(location, i, fill=text_color,font=font)
-    im.save("files/"+mail_list[count]+".png")
-    im.save("files/"+mail_list[count]+".pdf")
-    print(i, "----- Done---  ",mail_list[count])
+    im.save("pngs/"+mail_list[count]+".png")
+    im.save("pdfs/"+mail_list[count]+".pdf")
+    print(count+1,i, "----- Done---  ",mail_list[count])
     count=count+1
