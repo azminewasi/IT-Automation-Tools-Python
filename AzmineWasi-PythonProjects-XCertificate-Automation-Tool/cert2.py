ac05f6c824e4c8d540004abf1af23b748041eaa7
Azmine Toushik Wasi <azmine.wasi@gmail.com>
2022-03-23 20:56:17 +0600

    Modified

--- AzmineWasi-PythonProjects-XCertificate-Automation-Tool/cert2.py
+++ AzmineWasi-PythonProjects-XCertificate-Automation-Tool/cert2.py
@@ -2,38 +2,54 @@ from PIL import Image, ImageDraw, ImageFont
 import pandas as pd
 import cv2
 
-form = pd.read_excel("Attendees.xlsx")
+
+
+
+
+excel_file_name="Data.xlsx" 
+height_adjustment=30
+font_name="FONT.otf"
+text_color = (16,37,68)
+cert_demo="cert.png"
+
+fontXLtext=ImageFont.truetype(font_name, 40)
+fontLtext=ImageFont.truetype(font_name, 55)
+fontMtext=ImageFont.truetype(font_name, 72)
+fontNtext=ImageFont.truetype(font_name, 80)
+
+
+
+
+
+form = pd.read_excel(excel_file_name)
 name_list = form['Name'].to_list()
 mail_list = form['Mail'].to_list()
 
-
-img = cv2.imread('cert.png')
+img = cv2.imread(cert_demo)
 MAX_H,MAX_W,r = img.shape
-current_h, pad = (MAX_H/2)-30, 10
+current_h, pad = (MAX_H/2)-height_adjustment, 10
+
 
 print(img.shape)
 total = len(name_list)
-ei=0
-while (ei<total):
-	i=name_list[ei]
-	filenamess=mail_list[ei]+".png"
-	if (len(i)>35):
-		font = ImageFont.truetype("OLD.ttf", 40)
-		current_h, pad = (MAX_H/2), 10
-	elif (len(i)>25):
-		font = ImageFont.truetype("OLD.ttf", 55)
-	elif (len(i)>20):
-		font = ImageFont.truetype("OLD.ttf", 72)
-	else:
-		font = ImageFont.truetype("OLD.ttf", 80)
-
-
-	im = Image.open("cert.png")
-	d = ImageDraw.Draw(im)
-	w, h = d.textsize(i, font=font)
-	location = ((MAX_W - w) / 2, current_h)
-	text_color = (16,37,68)
-	d.text(location, i, fill=text_color,font=font)
-	im.save("files/"+filenamess)
-	print(i, "----- Done---  ",filenamess,"--", ei)
-	ei=ei+1
+count=0
+while (count<total):
+    i=name_list[count]
+    if (len(i)>35):
+        font = fontXLtext
+    elif (len(i)>25):
+        font = fontLtext
+    elif (len(i)>20):
+        font = fontMtext
+    else:
+        font = fontNtext
+        
+    im = Image.open(cert_demo)
+    d = ImageDraw.Draw(im)
+    w, h = d.textsize(i, font=font)
+    location = ((MAX_W - w) / 2, current_h)
+    d.text(location, i, fill=text_color,font=font)
+    im.save("files/"+mail_list[count]+".png")
+    im.save("files/"+mail_list[count]+".pdf")
+    print(i, "----- Done---  ",mail_list[count])
+    count=count+1
