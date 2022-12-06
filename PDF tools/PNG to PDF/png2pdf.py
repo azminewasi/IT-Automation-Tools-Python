bce7235cf29526363b5e96b160f4740962ac598b
Azmine Toushik Wasi <azmine.wasi@gmail.com>
2022-12-06 16:16:16 +0600

    File Edited

--- PDF tools/PNG to PDF/png2pdf.py
+++ PDF tools/PNG to PDF/png2pdf.py
@@ -6,21 +6,21 @@ import os
 
 # set here
 image_directory = ''
-extensions = ('*.jpg','*.png','*.gif') #add your image extentions
+extensions = ('*.jpg','*.png','*.jpeg') #add your image extentions
 # set 0 if you want to fit pdf to image
 # unit : pt
-margin = 10
+margin = 0
 
-imagelist=[]
-for ext in extensions:
-    imagelist.extend(glob.glob(os.path.join(image_directory,ext)))
+imagelist=["0.005.jpg","0.10.png"]
 
 for imagePath in imagelist:
     cover = Image.open(imagePath)
     width, height = cover.size
-pdf = FPDF(unit="pt", format=[width + 2*margin, height + 2*margin])
+    
+pdf = FPDF(unit="pt", format=[width + margin, height + margin])
 for imagePath in imagelist:
     pdf.add_page()
+    
     pdf.image(imagePath, margin, margin)
 
 pdf.output("data" + ".pdf", "F")
